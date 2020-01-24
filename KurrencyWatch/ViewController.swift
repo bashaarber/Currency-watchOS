8fe8c42f5aa4270c14bbd975aa32f4e1c6f807df
arberzs <arber@zombiesoup.com>
2020-01-24 22:46:52 +0100

    Initial Commit

---
+++ KurrencyWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KurrencyWatch
+//
+//  Created by Arber Basha on 24.1.20.
+//  Copyright © 2020 Arber Basha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
